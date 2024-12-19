185238dad4e561a552db917366a660ef972e380a
YavuzSelimAtes <yavuzslm60@gmail.com>
2024-12-19 20:36:39 +0300

    Müşteri kontroller ekledim

---
+++ WebKuaforProje/Controllers/MusteriController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebKuaforProje.Controllers
+{
+    public class MusteriController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
